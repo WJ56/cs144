721c6e70d30cc21f65c9de3031510ed183f93367
WJ56 <1271071660@qq.com>
2023-03-22 00:28:58 -0700

    'commit lab2'

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -12,21 +12,32 @@ using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
     DUMMY_CODE(seg);
+    if(!isn_flag && !seg.header().syn) return;
     TCPHeader header_tcpsegment = seg.header();
     Buffer payload_tcpsegment = seg.payload();
-    size_t length_tcpsegment = seg.length_in_sequence_space();
+    // size_t length_tcpsegment = seg.length_in_sequence_space();
     WrappingInt32 seqno_tcpsegment = header_tcpsegment.seqno;
     bool syn_tcpsegment = header_tcpsegment.syn;
     bool fin_tcpsegment = header_tcpsegment.fin;
 
+    string data = payload_tcpsegment.copy();
+    uint64_t checkpoint = _reassembler.stream_out().bytes_written();
+    if(syn_tcpsegment){
+        isn_flag = true;
+        isn = seqno_tcpsegment;
+        seqno_tcpsegment = operator+(seqno_tcpsegment, 1);
+    }
+    uint64_t index = unwrap(seqno_tcpsegment, isn, checkpoint) - 1;
+    _reassembler.push_substring(data, index, fin_tcpsegment);           // note: it is stream's index, it is beginning at zero.
 }
 
 optional<WrappingInt32> TCPReceiver::ackno() const { 
-
-    return {}; 
+    if(!isn_flag) return {};
+    size_t num = _reassembler.stream_out().bytes_written() + 1;
+    if(_reassembler.stream_out().input_ended()) num++;      // expected new data
+    return wrap(num, isn);
 }
 
 size_t TCPReceiver::window_size() const { 
-
-    return {}; 
+    return _reassembler.stream_out().remaining_capacity(); 
 }
